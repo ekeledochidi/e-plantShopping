eedb657dcfea36287234022a9924d48896650f94
ekeledochidi <ekeledochidiebere@gmail.com>
2025-04-08 17:41:25 +0100

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -290,7 +290,7 @@ function ProductList({ onHomeClick }) {
                 <div className="product-grid">
     {plantsArray.map((category, index) => (
     <div key={index}>
-        <h1><div>{category.category}</div></h1>
+        <h1><div style={{textAlign: 'center', border: '2px solid black'}}>{category.category}</div></h1>
         <div className="product-list">
             {category.plants.map((plant, plantIndex) => (
             <div className="product-card" key={plantIndex}>
