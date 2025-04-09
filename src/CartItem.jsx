a7f8a05469ed79d1004b11d804cb9d94e79e2cec
ekeledochidi <ekeledochidiebere@gmail.com>
2025-04-09 21:10:52 +0100

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -20,7 +20,9 @@ const CartItem = ({ onContinueShopping }) => {
    onContinueShopping(e)
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
     dispatch(increaseItem(item));
@@ -66,7 +68,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
